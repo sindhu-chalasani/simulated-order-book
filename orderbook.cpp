af2930b4250a695300abc7fc1b759f87e169fbf0
sindhu-chal <sindhu@apsdatatechnologies.com>
2025-10-11 02:24:42 -0400

    finished OrderModify class, started working on Trade class

--- orderbook.cpp
+++ orderbook.cpp
@@ -99,6 +99,40 @@ private:
 using OrderPointer = std::shared_ptr<Order>;
 using OrderPointers = std::list<OrderPointer>; //using list for simplicity instead of a vector, might change later
 
+class OrderModify
+{
+public: 
+    OrderModify(OrderId orderId, Side side, Price price, Quantity quantity)
+        : orderId_{ orderId }
+        , price_{ price }
+        , side_{ side }
+        , quantity_{ quantity }
+    { }
+
+    OrderId GetOrderId() const { return orderId_; }
+    Price GetPrice() const { return price_; }
+    Side GetSide() const { return side_; }
+    Quantity GetQuantity() const { return quantity_; }
+
+    OrderPointer ToOrderPointer(OrderType type) const
+    {
+        return std::make_shared<Order>(type, GetOrderId(), GetSide(), GetPrice(), GetQuantity());
+    };
+
+private:
+    OrderId orderId_;
+    Price price_;
+    Side side_;
+    Quantity quantity_;
+};
+
+struct TradeInfo
+{
+    OrderId orderId_;
+    Price price_;
+    Quantity quantity_;
+};
+
 int main()
 {
     return 0;
